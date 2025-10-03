3a5cf8a506a7d14047d0f916e5747e17dfb31b61
Harry <harryseymour.ulster@gamil.com>
2025-10-03 16:06:03 +0100

    Painting Objects to screen

--- src/Main/Java/Game.java
+++ src/Main/Java/Game.java
@@ -1,7 +1,19 @@
 package Java;
 
+import javax.swing.*;
+
 public class Game {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("2D Adventure");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
 
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
--- src/Main/Java/GamePanel.java
+++ src/Main/Java/GamePanel.java
@@ -1,7 +1,53 @@
 package Java;
 
 import javax.swing.*;
+import java.awt.*;
+                                      // Allows us to use threading
+public class GamePanel extends JPanel implements Runnable{
 
-public class GamePanel extends JPanel {
+    // Screen Settings
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale; // 48x48 - Actual tile size displayed on screen
+    final int maxScreenColumn = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenColumn; // 768 pixels - 16 * 48
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels - 16 * 12
 
+    Thread gameThread;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true); // improves game rendering performance
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this); // Passing this GamePanel class into Thread constructor
+        gameThread.start(); // class run method
+    }
+
+    @Override // Inherited method from runnable interface that runs every frame
+    public void run() {
+        // Game Loop
+        while(gameThread != null){
+            update();
+            repaint(); // calls paintComponent method
+        }
+    }
+
+    // Updates positions
+    public void update(){
+
+    }
+
+    // Updates positions to the screen
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g; // convert graphics -> graphics 2D
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+
+        g2.dispose();
+    }
 }
